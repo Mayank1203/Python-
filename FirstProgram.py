314c5498ed43cfd50bc0d210aa8de100b5a16e5a
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-24 11:01:25 +0530

    basic syntax of python

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,11 @@
-print("This is First Python Code")
+# print("This is First Python Code", "Mayank")
+
+name = "Mayank"
+
+age = 22
+age1 = 34
+
+print(type(age))
+print(type(name))
+
+print(age+age1)
