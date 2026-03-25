68111c96dae4c645baa01724bec5d84240c643b4
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-25 14:49:10 +0530

    Questions solved

--- PraticeQuestions.py
+++ PraticeQuestions.py
@@ -6,8 +6,15 @@ b = int(input("Enter Second Number : "))
 sum = a+b
 print("Sum of numbers : " , sum)
 '''
-
+'''
 # WAP to input side of square & print its area
 
 side = float(input("Enter side of Square : "))
 print("Area of Square : " , side*side)
+'''
+
+# WAP to input user's first name & print its length
+name = input("Enter your name :")
+print("length of name : " , len(name))
+print(name.find('e'))
+print(name.count('e'))
