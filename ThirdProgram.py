5167f4d7a8c19d013e4b0237e13498e62a2825a2
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-26 19:00:48 +0530

    List functions

--- ThirdProgram.py
+++ ThirdProgram.py
@@ -10,7 +10,36 @@ mix[0] = "tyuiop"
 print(mix)
 '''
 
+'''
 nums = [1,5,7,9,3,4]
 print(nums)
 print(len(nums))
 print(nums[1:4])#Slicing(ending index excluded)
+'''
+
+
+#List Methods 
+list = ['w' , 'd' , 's','f','c','k']
+list.append('x') # adds one element at the end
+print(list)
+
+list.sort()
+print(list)# sort in ascending order
+
+list.sort(reverse=True) #sort in descending order
+print(list)
+
+list.reverse() #reverse the list
+print(list)
+
+list.insert(3,'a')
+print(list)
+
+list.remove('d') #remove first occurence of element 
+print(list)
+
+list.pop(5) #remove element of particular index
+print(list)
+
+
+
