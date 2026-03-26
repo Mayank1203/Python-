d607668e3a9641b8d1eec9c2b62933d92e7c15f5
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-26 18:41:29 +0530

    Slicing in List

--- ThirdProgram.py
+++ ThirdProgram.py
@@ -1,3 +1,4 @@
+'''
 marks = [1,2,3,4,5]# this is a list
 print(marks)
 print(len(marks))
@@ -7,3 +8,9 @@ mix = ["abcd" , 345 , "ghjk"]# this is a list
 print(mix)
 mix[0] = "tyuiop"
 print(mix)
+'''
+
+nums = [1,5,7,9,3,4]
+print(nums)
+print(len(nums))
+print(nums[1:4])#Slicing(ending index excluded)
