a91b4b80d083128ca794a5513bbe7508bf244a37
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-26 18:36:22 +0530

    Implementation of List and Indexing

---
+++ ThirdProgram.py
@@ -0,0 +1,9 @@
+marks = [1,2,3,4,5]# this is a list
+print(marks)
+print(len(marks))
+print(marks[3]) # Indexing 
+
+mix = ["abcd" , 345 , "ghjk"]# this is a list
+print(mix)
+mix[0] = "tyuiop"
+print(mix)
