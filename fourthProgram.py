fa12185a618778b6afd39837ca8bd3c3c25c1395
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-27 16:14:21 +0530

    Empty Dict & Adding values

--- fourthProgram.py
+++ fourthProgram.py
@@ -1,18 +1,23 @@
-info = {
-    "name" : "Raja",
-    "subjects" : ["python" , "Java"],
-    "topics" : {"dict", "set"},
-    "age" : 23,
-    "is_Adult" : True
-}
+# info = {
+#     "name" : "Raja",
+#     "subjects" : ["python" , "Java"],
+#     "topics" : {"dict", "set"},
+#     "age" : 23,
+#     "is_Adult" : True
+# }
 
 # print(info)
 # print(type(info))
 
-info["name"] = "gghjdfl" # change in name 
-print(info["name"]) # Accessing values ny providing keys
-print(info["subjects"])
-print(info["topics"])
+# info["name"] = "gghjdfl" # change in name 
+# print(info["name"]) # Accessing values ny providing keys
+# print(info["subjects"])
+# print(info["topics"])
 
+EmptyDict = {
+}
 
+print(EmptyDict)
+EmptyDict["name"] = "Hello"
+print(EmptyDict)
 
