d6b3838a58a1e859f03021da001fce7df38ba986
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-28 07:50:34 +0530

    break & Continue keywords

--- FifthProgram.py
+++ FifthProgram.py
@@ -2,5 +2,17 @@ num = 1 # Iterator
 
 while num <= 10 : # Iteration 
     print(num)
+    if(num == 9):
+        break
     num += 1
+
+
+i = 1 
+
+while i<=10 :
+    if(i % 3 == 0):
+        i+=1
+        continue
+    print(i)
+    i+=1
 
