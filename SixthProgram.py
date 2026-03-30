1dd6801bc9cdeefa74acaa9e04d61f8939b75edf
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-30 15:57:23 +0530

    User & Build-In functions

--- SixthProgram.py
+++ SixthProgram.py
@@ -2,15 +2,26 @@
 
 # Function Definition 
 
-def calcSum(a,b): # a,b are the parameters 
-    return a+b 
+# def calcSum(a,b): # a,b are the parameters 
+#     return a+b 
 
-print(calcSum(2,6)) # Function calling & (2 and 6) both are arguments
-print(calcSum(34782,667814))
+# print(calcSum(2,6)) # Function calling & (2 and 6) both are arguments
+# print(calcSum(34782,667814))
 
-def printHello():
-    print("hello")
+# def printHello():
+#     print("hello")
 
-printHello()
-printHello()
+# printHello()
+# printHello()
+
+def Average(a,b,c): # User defined Functions 
+    avg = (a+b+c)/3 
+    return avg
+
+avgNum = Average(1,2,3)
+print(avgNum) 
+
+# Build in functions 
+    # len() , print() , type() etc..def
+
 
