78171e141bb62ef52a96056f56bfe26830faa63c
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-30 23:19:20 +0530

    overridding & append in file

--- SeventhProgram.py
+++ SeventhProgram.py
@@ -1,14 +1,34 @@
 # I/O file in Python
 
-f = open("demo.txt","r")
+'''
+# f = open("demo.txt","r")
 
 # data = f.read()
 # print(data)
+'''
 
-line1 = f.readline()
-print(line1)
+'''
+# line1 = f.readline()
+# print(line1)
 
-line2 = f.readline()
-print(line2)
+# line2 = f.readline()
+# print(line2)
+
+# f.close()
+'''
+'''
+# f = open("demo.txt","w") # w means overriding the existing once
+
+# f.write("I have to complete this in 2 days only")
+
+# f.close()
+'''
+
+
+'''
+f = open("demo.txt","a") # a means add the existing once
+
+f.write("\n Tomorrow is the last day")
 
 f.close()
+'''
