14e353f27fd6cac83a6373064afcca480a217d0d
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-30 12:39:13 +0530

    Basic terminologies used in Function

--- SixthProgram.py
+++ SixthProgram.py
@@ -1,1 +1,16 @@
 # Functions in Python 
+
+# Function Definition 
+
+def calcSum(a,b): # a,b are the parameters 
+    return a+b 
+
+print(calcSum(2,6)) # Function calling & (2 and 6) both are arguments
+print(calcSum(34782,667814))
+
+def printHello():
+    print("hello")
+
+printHello()
+printHello()
+
