f59facc66038be3c1c82421e037b0bac7c3e902d
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-30 23:10:43 +0530

    read file line by line

--- SeventhProgram.py
+++ SeventhProgram.py
@@ -2,10 +2,13 @@
 
 f = open("demo.txt","r")
 
-data = f.read()
-print(data)
+# data = f.read()
+# print(data)
 
-# line1 = f.readline()
-# print(line1)
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
 
 f.close()
