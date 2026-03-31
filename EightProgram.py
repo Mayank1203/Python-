045225e100323594d03f3df8d8ec45b5f2413819
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-31 13:55:43 +0530

    Default and Parameterised constructor

--- EightProgram.py
+++ EightProgram.py
@@ -1,9 +1,23 @@
 # Class & Object in Python
+'''
+# class Car :
+#     name = "M5 zxm"
+#     brand = "BMW"
 
-class Car :
-    name = "M5 zxm"
-    brand = "BMW"
+# car1 = Car()
+# print(car1.name)
+# print(car1.brand)
+'''
 
-car1 = Car()
-print(car1.name)
-print(car1.brand)
+class Students :
+    # def __init__(self):
+    #     print("default connstructor")
+
+    def __init__(self , name , marks):
+        self.name = name
+        self.marks = marks
+        print("parameterised constructor")
+
+s1 = Students("karan" , 93)
+print(s1.name , s1.marks)
+
