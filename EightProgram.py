044d8b68744cdbf8a51573532b2d472ba7e467ca
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-31 17:21:14 +0530

    methods implementation

--- EightProgram.py
+++ EightProgram.py
@@ -19,9 +19,18 @@ class Students :
         self.marks = marks
         print("parameterised constructor")
 
-s1 = Students("karan" , 93)
-print(s1.name , s1.marks)
-print(s1.college_name)
+    def welcome(self): # Method
+        print("welcome" , self.name)
+
+    def getMarks(self): # Method
+        return self.marks 
+
+# s1 = Students("karan" , 93)
+# print(s1.name , s1.marks)
+# print(s1.college_name)
 
 # Object Attribute have higher presidence as compare to Class Attribute
 
+s2 = Students("maya",89)
+s2.welcome()
+print(s2.getMarks())
